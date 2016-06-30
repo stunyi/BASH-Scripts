66a64d30c2f71e8df3758d252bf527799bfe1bd2
Stunyi <michael.munyi@gmunyi.com>
2016-06-30 12:09:03 -0400

    Made changes to the df function to only work with sda disks

--- 01-display-system-details.sh
+++ 01-display-system-details.sh
@@ -23,12 +23,12 @@ echo "Main memory: $used_memory / $total_memory"
 # Show all the Disks attached to machine and there space
 echo "List of Available disks"
 echo "====================================="
-for disk in $(df -h . | grep dev | awk '{print $1}')
+for disk in $(df -h | grep "\/dev\/sda" | awk '{print $1}')
 do
-percentage_use=$(df -h . | grep dev | awk '{print $5}')
-total_disk_space=$(df -h . | grep dev | awk '{print $2}')
-actual_disk_usage=$(df -h . | grep dev | awk '{print $3}')
-actual_disk_unused=$(df -h . | grep dev | awk '{print $4}')
+percentage_use=$(df -h | grep "\/dev\/sda" | awk '{print $5}')
+total_disk_space=$(df -h | grep "\/dev\/sda" | awk '{print $2}')
+actual_disk_usage=$(df -h | grep "\/dev\/sda" | awk '{print $3}')
+actual_disk_unused=$(df -h | grep "\/dev\/sda" | awk '{print $4}')
 echo "Disk: $disk, space:  $actual_disk_usage/$total_disk_space($percentage_use)"
 done
 
